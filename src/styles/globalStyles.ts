e1f430c5b9df675bd691885d4e1483c956647698
SangWonLee <tkddnjs0687@naver.com>
2024-04-28 23:16:27 +0900

    style: 글로벌스타일 수정

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,8 +1,39 @@
+import emotionReset from 'emotion-reset';
 import { css } from '@emotion/react';
 
-const globalStyles = css`
+const globalStyles = () => css`
+  ${emotionReset};
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html,
+  body {
+    height: 100%;
+  }
   html {
     font-size: 62.5%;
+  }
+  body {
+    overflow-y: overlay;
+  }
+
+  #root {
+    width: 100%;
+    height: 100%;
+    font-family: 'Pretendard', sans-serif;
   }
 
   a {
@@ -19,6 +50,15 @@ const globalStyles = css`
   ol,
   ul {
     list-style: none;
+  }
+  button {
+    border: 0;
+    background-color: transparent;
+    cursor: pointer;
+
+    &:disabled {
+      cursor: not-allowed;
+    }
   }
 `;
 
