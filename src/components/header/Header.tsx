f64ad48fb20c8ee47bfc4cd124e7ccf446bcdfef
SangWonLee <tkddnjs0687@naver.com>
2024-04-28 23:17:37 +0900

    feat: Header 컴포넌트 제작

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './Header.styled';
+
+const Header = () => {
+  return (
+    <S.Container>
+      <S.Wrapper>Header</S.Wrapper>
+    </S.Container>
+  );
+};
+
+export default Header;
