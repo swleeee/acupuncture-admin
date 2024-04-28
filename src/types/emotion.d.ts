e3b8fd23dbed5caee15d628ecaa1327967895a48
SangWonLee <tkddnjs0687@naver.com>
2024-04-28 23:13:53 +0900

    chore: emotion-reset 라이브러리 설치 및 설정

--- src/types/emotion.d.ts
+++
@@ -1,9 +0,0 @@
-import "@emotion/react";
-
-import { theme } from "@/styles";
-
-type ThemeType = typeof theme;
-
-declare module "@emotion/react" {
-  export interface Theme extends ThemeType {}
-}
